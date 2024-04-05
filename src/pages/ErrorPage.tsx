b18688790537f2a0a3777a0556569d4818a9c151
Konrad Milewski <konradmilewski2000@interia.eu>
2024-04-06 00:08:13 +0200

    Add tailwindcss, router, Clear some files

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,26 @@
+// @ts-check
+/** @type {number} */
+
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = (): JSX.Element => {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div id="error-page" className="">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h1>Oops!</h1>
+        <h4>Sorry, unexpected error has occured.</h4>
+        <p>
+          {error.StatusCode} {error.StatusText || error.message}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
