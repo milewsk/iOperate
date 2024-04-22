339d49eaae577d6c858f8302ef1733abb553ae7f
Konrad Milewski <konradmilewski2000@interia.eu>
2024-04-22 19:08:26 +0200

    Add error page logic

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,18 @@
-// @ts-check
-/** @type {unknown} */
-
-import { useRouteError } from "react-router-dom";
+import { useState } from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const ErrorPage = (): JSX.Element => {
+  const [errorStatus, setErrorStatus] = useState<number | null>(null);
+  const [statusText, setStatusText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const error = useRouteError();
   console.log(error);
+
+  if (isRouteErrorResponse(error)) {
+    setErrorStatus(error.status);
+    setStatusText(error.statusText);
+    setErrorMessage(error.data);
+  }
 
   return (
     <div id="error-page" className="">
@@ -16,7 +23,7 @@ const ErrorPage = (): JSX.Element => {
         <h1>Oops!</h1>
         <h4>Sorry, unexpected error has occured.</h4>
         <p>
-          {error.StatusCode} {error.StatusText || error.message}
+          {errorStatus ?? ""} {(statusText ?? "") || (errorMessage ?? "")}
         </p>
       </div>
     </div>
